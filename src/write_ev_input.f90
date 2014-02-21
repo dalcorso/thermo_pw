0f086ba9969d4356a64cc5ecbdb211347e4e24f3
dalcorso <dalcorso>
2014-02-21 15:14:59 +0000

    The volume is always used in the Murnaghan input. The relationship between lattice constant and volume generalized to arbitrary geometries.

--- src/write_ev_input.f90
+++ src/write_ev_input.f90
@@ -17,7 +17,7 @@ SUBROUTINE write_ev_input(file_dat)
   USE kinds, ONLY : DP
   USE mp_images, ONLY : my_image_id, root_image, nproc_image
   USE mp_world,  ONLY : world_comm
-  USE thermo_mod, ONLY : alat_geo, energy_geo, ngeo
+  USE thermo_mod, ONLY : omega_geo, energy_geo, ngeo
   USE io_global, ONLY : ionode
   IMPLICIT NONE
   CHARACTER(LEN=256) :: file_dat
@@ -33,7 +33,7 @@ SUBROUTINE write_ev_input(file_dat)
      iu_ev=2
      OPEN(UNIT=iu_ev, FILE=TRIM(file_dat), STATUS='UNKNOWN', FORM='FORMATTED')
      DO igeom=1,ngeo
-        WRITE(iu_ev,'(2f20.10)') alat_geo(igeom), energy_geo(igeom)
+        WRITE(iu_ev,'(2e30.15)') omega_geo(igeom), energy_geo(igeom)
      ENDDO
      !
      CLOSE(iu_ev)
@@ -54,7 +54,7 @@ IF (ionode) THEN
    iu_ev=2
    OPEN(UNIT=iu_ev, FILE='input_ev', STATUS='UNKNOWN', FORM='FORMATTED')
    WRITE(iu_ev,'("au")')
-   WRITE(iu_ev,'("fcc")')
+   WRITE(iu_ev,'("hex")')
    WRITE(iu_ev,'("4")')
    WRITE(iu_ev,'(a)') TRIM(file_dat)
    WRITE(iu_ev,'(a)') TRIM(file_dat)//'.ev.out'
@@ -64,11 +64,12 @@ ENDIF
 RETURN
 END SUBROUTINE write_ev_driver
 
-SUBROUTINE do_ev()
+SUBROUTINE do_ev(itry)
 !
 !  This subroutine compute the equilibrium volume and bulk modulus
 !
 USE kinds, ONLY : DP
+USE thermo_mod, ONLY : omega_geo, alat_geo, ngeo
 USE control_mur, ONLY : vmin, b0, b01, emin
 USE control_thermo, ONLY : flevdat
 USE io_global, ONLY : meta_ionode_id, stdout
@@ -76,9 +77,12 @@ USE mp_world, ONLY : world_comm
 USE mp, ONLY : mp_bcast
 
 IMPLICIT NONE
+INTEGER, INTENT(IN) :: itry
 CHARACTER(LEN=256) :: file_dat
+CHARACTER(LEN=6) :: int_to_char
+REAL(DP) :: compute_alat_geo
 
-  file_dat=TRIM(flevdat)
+  file_dat=TRIM(flevdat) // TRIM(int_to_char(itry))
   CALL write_ev_input(file_dat)
   CALL ev_sub(vmin, b0, b01, emin)
   CALL mp_bcast(vmin, meta_ionode_id, world_comm)
@@ -88,7 +92,7 @@ CHARACTER(LEN=256) :: file_dat
   !
   WRITE(stdout,'(/,2x,76("+"))')
   WRITE(stdout,'(5x, "The equilibrium lattice constant is ",f12.4," a.u.")') &
-                                           (vmin * 4.0_DP)**(1.0_DP/3.0_DP)
+               compute_alat_geo(vmin, alat_geo(ngeo/2+1), omega_geo(ngeo/2+1))
   WRITE(stdout,'(5x, "The bulk modulus is ",15x,f12.2," kbar")')  b0
   WRITE(stdout,'(2x,76("+"),/)')
 
@@ -99,7 +103,7 @@ SUBROUTINE do_ev_t(itemp)
 !  This subroutine compute the equilibrium volume and bulk modulus
 !
 USE kinds,          ONLY : DP
-USE thermo_mod,     ONLY : ngeo, alat_geo, energy_geo
+USE thermo_mod,     ONLY : ngeo, alat_geo, omega_geo, energy_geo
 USE thermodynamics, ONLY : ph_free_ener
 USE anharmonic,     ONLY : vmin_t, b0_t, b01_t, free_e_min_t
 USE temperature,    ONLY : ntemp, temp
@@ -112,6 +116,7 @@ INTEGER, INTENT(IN) :: itemp
 INTEGER :: igeom, iu_ev
 CHARACTER(LEN=256) :: file_dat
 CHARACTER(LEN=6) :: int_to_char
+REAL(DP) :: compute_alat_geo
 REAL(DP) :: free_e, vm, b0, b01
 
   IF (my_image_id /= root_image) RETURN
@@ -123,7 +128,7 @@ REAL(DP) :: free_e, vm, b0, b01
      iu_ev=2
      OPEN(UNIT=iu_ev, FILE=TRIM(file_dat), STATUS='UNKNOWN', FORM='FORMATTED')
      DO igeom=1,ngeo
-        WRITE(iu_ev,'(2f20.10)') alat_geo(igeom), energy_geo(igeom) + &
+        WRITE(iu_ev,'(2f20.10)') omega_geo(igeom), energy_geo(igeom) + &
                                             ph_free_ener(itemp,igeom)
      ENDDO
      !
@@ -140,7 +145,7 @@ REAL(DP) :: free_e, vm, b0, b01
   WRITE(stdout,'(/,2x,76("-"))')
   WRITE(stdout,'(5x, "phdos free energy, at T= ", f12.6)') temp(itemp)
   WRITE(stdout,'(5x, "The equilibrium lattice constant is ",f12.6," a.u.")') &
-                             (vmin_t(itemp)*4.0_DP)**(1.0_DP/3.0_DP)
+      compute_alat_geo(vmin_t(itemp), alat_geo(ngeo/2+1), omega_geo(ngeo/2+1))
   WRITE(stdout,'(5x, "The bulk modulus is ",15x,f12.4," kbar")')  b0_t(itemp)
   WRITE(stdout,'(5x, "The bulk modulus derivative is ",15x,f12.4)')  b01_t(itemp)
   WRITE(stdout,'(2x,76("-"),/)')
@@ -152,7 +157,7 @@ SUBROUTINE do_ev_t_ph(itemp)
 !  This subroutine compute the equilibrium volume and bulk modulus
 !
 USE kinds,          ONLY : DP
-USE thermo_mod,     ONLY : ngeo, alat_geo, energy_geo
+USE thermo_mod,     ONLY : ngeo, alat_geo, omega_geo, energy_geo
 USE ph_freq_thermodynamics, ONLY : phf_free_ener
 USE ph_freq_anharmonic,     ONLY : vminf_t, b0f_t, b01f_t, free_e_minf_t
 USE temperature,    ONLY : ntemp, temp
@@ -166,6 +171,7 @@ INTEGER :: igeom, iu_ev
 CHARACTER(LEN=256) :: file_dat
 CHARACTER(LEN=6) :: int_to_char
 REAL(DP) :: free_e, vm, b0, b01
+REAL(DP) :: compute_alat_geo
 
   IF (my_image_id /= root_image) RETURN
 
@@ -176,7 +182,7 @@ REAL(DP) :: free_e, vm, b0, b01
      iu_ev=2
      OPEN(UNIT=iu_ev, FILE=TRIM(file_dat), STATUS='UNKNOWN', FORM='FORMATTED')
      DO igeom=1,ngeo
-        WRITE(iu_ev,'(2f20.10)') alat_geo(igeom), energy_geo(igeom) + &
+        WRITE(iu_ev,'(2f20.10)') omega_geo(igeom), energy_geo(igeom) + &
                                             phf_free_ener(itemp,igeom)
      ENDDO
      !
@@ -193,7 +199,7 @@ REAL(DP) :: free_e, vm, b0, b01
   WRITE(stdout,'(/,2x,76("+"))')
   WRITE(stdout,'(5x, "ph_freq free energy at T=",f12.4)') temp(itemp)
   WRITE(stdout,'(5x, "The equilibrium lattice constant is ",f12.6," a.u.")') &
-                                 (vminf_t(itemp)*4.0_DP)**(1.0_DP/3.0_DP)
+      compute_alat_geo(vminf_t(itemp), alat_geo(ngeo/2+1), omega_geo(ngeo/2+1))
   WRITE(stdout,'(5x, "The bulk modulus is ",15x,f12.4," kbar")')  b0f_t(itemp)
   WRITE(stdout,'(5x, "The bulk modulus derivative is ",15x,f12.4)')  b01f_t(itemp)
   WRITE(stdout,'(2x,76("+"),/)')
