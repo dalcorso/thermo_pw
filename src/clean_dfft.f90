45f85b06b4d883109acf456a9c15b3f656bff66d
dalcorso <dalcorso>
2014-07-25 16:41:38 +0000

    fft dimensions reinitialized to the parameters read on input.

--- src/clean_dfft.f90
+++ src/clean_dfft.f90
@@ -8,15 +8,16 @@
 SUBROUTINE clean_dfft()
 
 USE fft_base, ONLY : dfftp, dffts
+USE input_parameters, ONLY : nr1, nr2, nr3, nr1s, nr2s, nr3s
 
 IMPLICIT NONE
 
-dfftp%nr1=0
-dfftp%nr2=0
-dfftp%nr3=0
-dffts%nr1=0
-dffts%nr2=0
-dffts%nr3=0
+dfftp%nr1=nr1
+dfftp%nr2=nr2
+dfftp%nr3=nr3
+dffts%nr1=nr1s
+dffts%nr2=nr2s
+dffts%nr3=nr3s
 
 RETURN
 END SUBROUTINE clean_dfft
