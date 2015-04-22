627f2fdb3e4d4f9e9bfb5c9092ed626f02c39e52
dalcorso <dalcorso>
2015-04-22 13:11:11 +0000

    Added a module to calculate isoentropic or isobaric quantities. compute_cp modified to use the routines of the module.

---
+++ lib/isoentropic.f90
@@ -0,0 +1,223 @@
+!
+! Copyright (C) 2015 Andrea Dal Corso 
+! This file is distributed under the terms of the
+! GNU General Public License. See the file `License'
+! in the root directory of the present distribution,
+! or http://www.gnu.org/copyleft/gpl.txt .
+!
+MODULE isoentropic
+!
+!   this module contains the support routines for the calculation
+!   of the isoentropic quantities given the corresponding isothermal
+!   quantities and the parameters that link the two.
+!   It can calculate:
+!   The isoentropic elastic constants given the isothermal elastic constants,
+!   the isoentropic bulk modulus given the isothermal bulk modulus,
+!   the isobaric specific heat given the isochoric one,
+!   the average gruneisen parameter.
+!   The routine does not receive the isothermal quantities and
+!   provide only the difference between isoentropic and isothermal
+!   quantities. 
+!
+!
+  USE kinds,     ONLY : DP
+  USE io_global, ONLY : stdout, ionode, ionode_id
+  USE mp_images, ONLY : intra_image_comm
+  USE mp,        ONLY : mp_bcast 
+  IMPLICIT NONE
+  PRIVATE
+  SAVE
+
+  PUBLIC  isobaric_heat_capacity, isoentropic_bulk_modulus,  &
+          average_gruneisen, thermal_stress,                 &
+          isoentropic_elastic_constants, gen_average_gruneisen
+
+CONTAINS
+!
+SUBROUTINE isobaric_heat_capacity(volume,b0_t,beta_t,temp,cpmcv,ntemp)
+!
+!  This routine receives the volume, the bulk modulus, the volume thermal
+!  expansion at ntemp temperatures and provides the difference between
+!  isobaric and isochoric heat capacity.
+!
+USE kinds, ONLY : DP
+USE constants, ONLY : ry_kbar
+IMPLICIT NONE
+
+INTEGER :: ntemp
+REAL(DP), INTENT(IN) :: volume(ntemp), b0_t(ntemp), beta_t(ntemp), &
+                        temp(ntemp)
+REAL(DP), INTENT(INOUT) :: cpmcv(ntemp)
+
+INTEGER :: itemp
+
+cpmcv=0.0_DP
+DO itemp=2, ntemp-1
+   cpmcv(itemp) = temp(itemp) * volume(itemp) * &
+                           beta_t(itemp)**2 * b0_t(itemp) / ry_kbar
+END DO
+
+RETURN
+END SUBROUTINE isobaric_heat_capacity
+
+SUBROUTINE isoentropic_bulk_modulus(volume,b0_t,beta_t,cp_t,temp,bsmbt,ntemp)
+!
+!  This routine receives the volume, the bulk modulus, the volume thermal
+!  expansion and the isobaric heat capacity at ntemp temperatures and 
+!  provides the difference between isoentropic and isothermal bulk modulus
+!
+
+USE kinds, ONLY : DP
+USE constants, ONLY : ry_kbar
+IMPLICIT NONE
+INTEGER, INTENT(IN) :: ntemp
+REAL(DP), INTENT(IN) :: volume(ntemp), b0_t(ntemp), beta_t(ntemp), &
+                        cp_t(ntemp), temp(ntemp)
+REAL(DP), INTENT(INOUT) :: bsmbt(ntemp)
+
+INTEGER :: itemp
+
+bsmbt=0.0_DP
+DO itemp=2,ntemp-1
+   bsmbt(itemp) =  temp(itemp) * volume(itemp) * beta_t(itemp)**2 * &
+                   b0_t(itemp)**2 / cp_t(itemp) / ry_kbar
+END DO
+
+RETURN
+END SUBROUTINE isoentropic_bulk_modulus
+
+SUBROUTINE average_gruneisen(volume,b0_t,beta_t,cv_t,temp,gamma_t,ntemp)
+!
+!  This routine receives the volume, the bulk modulus, the volume thermal
+!  expansion and the isochoric heat capacity at ntemp temperatures and 
+!  provides the average Gruneisen parameter
+!
+USE kinds, ONLY : DP
+USE constants, ONLY : ry_kbar
+IMPLICIT NONE
+INTEGER, INTENT(IN) :: ntemp
+REAL(DP), INTENT(IN) :: volume(ntemp), b0_t(ntemp), beta_t(ntemp), &
+                        cv_t(ntemp), temp(ntemp)
+REAL(DP), INTENT(OUT) :: gamma_t(ntemp)
+
+INTEGER :: itemp
+
+gamma_t=0.0_DP
+DO itemp=2,ntemp-1
+   gamma_t(itemp)=beta_t(itemp) * b0_t(itemp) * volume(itemp) / cv_t(itemp) &
+                                              / ry_kbar
+END DO
+
+RETURN
+END SUBROUTINE average_gruneisen
+
+SUBROUTINE thermal_stress(el_con_t,alpha_t,bths,ntemp)
+!
+!  This routine receives the isothermal elastic constants and
+!  the thermal expansion tensor and gives as output the thermal stress as a
+!  function of temperature. 
+!  Elastic constants and thermal expansion in input are in Voigt 
+!  notation, but thermal stresses are given in standard notation as a
+!  3x3 matrix.
+!  On output thermal stresses are in the same units of the elastic
+!  constants
+!
+
+USE kinds, ONLY : DP
+USE constants, ONLY : ry_kbar
+USE elastic_constants, ONLY : el_cons_voigt, trans_epsilon
+IMPLICIT NONE
+INTEGER, INTENT(IN) :: ntemp
+REAL(DP), INTENT(IN) :: el_con_t(6,6,ntemp), alpha_t(6,ntemp) 
+REAL(DP), INTENT(OUT) :: bths(3,3,ntemp)
+
+REAL(DP) :: elcon(3,3,3,3), alp(3,3), aux(6,6)
+INTEGER :: itemp, i,j,m,n
+
+bths=0.0_DP
+DO itemp=2,ntemp-1
+   aux(:,:)=el_con_t(:,:,itemp)
+   CALL el_cons_voigt(aux, elcon, .FALSE.)
+   CALL trans_epsilon(alpha_t(1,itemp), alp, 1)
+   DO i=1,3 
+      DO j=1,3
+         DO m=1,3
+            DO n=1,3
+               bths(i,j,itemp) = bths(i,j,itemp) - elcon(i,j,m,n) * alp(m,n)
+            END DO
+         END DO
+      END DO
+   END DO
+END DO
+
+RETURN
+END SUBROUTINE thermal_stress
+
+SUBROUTINE isoentropic_elastic_constants(volume,bths,cv_t,temp,csmct,ntemp)
+!
+!  This routine receives the thermal stress, in kbar, the specific heat
+!  in Ry/cell and gives as output the correction to the elastic constants
+!  in kbar
+!
+USE kinds, ONLY : DP
+USE constants, ONLY : ry_kbar
+USE elastic_constants, ONLY : el_cons_voigt
+IMPLICIT NONE
+INTEGER, INTENT(IN) :: ntemp
+REAL(DP), INTENT(IN) :: volume(ntemp), bths(3,3,ntemp), cv_t(ntemp), &
+                        temp(ntemp)
+REAL(DP), INTENT(INOUT) :: csmct(6,6,ntemp)
+
+REAL(DP) :: aux(3,3,3,3)
+INTEGER :: itemp, i, j, m, n
+
+csmct=0.0_DP
+DO itemp=2,ntemp-1
+   DO i=1,3
+      DO j=1,3
+         DO m=1,3
+            DO n=1,3
+               aux(i,j,m,n)= - temp(itemp) * volume(itemp) * bths(i,j,itemp)* &
+                               bths(m,n,itemp) / cv_t(itemp) / ry_kbar
+            END DO
+         END DO
+      END DO
+   END DO 
+!
+!  transform to voigt indeces and copy aux in csmct
+!
+   CALL el_cons_voigt(csmct(1,1,itemp), aux, .TRUE.) 
+END DO
+
+RETURN
+END SUBROUTINE isoentropic_elastic_constants
+
+SUBROUTINE gen_average_gruneisen(volume,bths,cv_t,temp,ggamma_t,ntemp)
+!
+!  This routine receives the thermal stress, the constant strain specific
+!  heat and gives the generalized average Gruneisen parameters.
+!  Note that we use here the definition reported in 
+!  D.C. Wallace, Thermodynamics of Crystals.
+!
+USE kinds, ONLY : DP
+IMPLICIT NONE
+INTEGER, INTENT(IN) :: ntemp
+REAL(DP), INTENT(IN) :: volume(ntemp), bths(3,3,ntemp), cv_t(ntemp), &
+                        temp(ntemp)
+REAL(DP), INTENT(INOUT) :: ggamma_t(3,3,ntemp)
+
+INTEGER :: itemp, i, j
+
+ggamma_t=0.0_DP
+DO itemp = 2,ntemp-1
+   DO i = 1,3
+      DO j = 1,3
+         ggamma_t(i,j,itemp) = volume(itemp) * bths(i,j,itemp) / cv_t(itemp)
+      END DO
+   END DO 
+END DO
+
+RETURN
+END SUBROUTINE gen_average_gruneisen
+
+END MODULE isoentropic
--- src/compute_cp.f90
+++ src/compute_cp.f90
@@ -1,5 +1,5 @@
 !
-! Copyright (C) 2014 Andrea Dal Corso
+! Copyright (C) 2014-2015 Andrea Dal Corso
 ! This file is distributed under the terms of the
 ! GNU General Public License. See the file `License'
 ! in the root directory of the present distribution,
@@ -19,13 +19,15 @@ SUBROUTINE compute_cp(beta_t, vmin_t, b0_t, ph_cv, cv_t, cp_t, b0_s, gamma_t)
 !  gamma_t adimensional
 !
 USE kinds,          ONLY : DP
-USE thermo_mod,     ONLY : tot_ngeo, omega_geo
+USE thermo_mod,     ONLY : ngeo, omega_geo
 USE temperature,    ONLY : temp, ntemp
+USE isoentropic,    ONLY : isobaric_heat_capacity, isoentropic_bulk_modulus, &
+                           average_gruneisen
 USE constants,      ONLY : ry_kbar
 
 IMPLICIT NONE
 REAL(DP), INTENT(IN)  :: beta_t(ntemp), vmin_t(ntemp), b0_t(ntemp), &
-                         ph_cv(ntemp, tot_ngeo)
+                         ph_cv(ntemp, ngeo(1))
 REAL(DP), INTENT(OUT) :: cv_t(ntemp), cp_t(ntemp), b0_s(ntemp), gamma_t(ntemp)
 INTEGER :: itemp, igeo, igeo1, igeo2
 
@@ -35,7 +37,7 @@ DO itemp=1,ntemp
 !
    igeo1=1
    igeo2=2
-   DO igeo = 1, tot_ngeo-1
+   DO igeo = 1, ngeo(1)-1
       IF ( vmin_t(itemp) > omega_geo(igeo) ) THEN
          igeo1=igeo
          igeo2=igeo1+1
@@ -48,20 +50,20 @@ DO itemp=1,ntemp
                  ph_cv(itemp,igeo1) ) * (vmin_t(itemp)-omega_geo(igeo1)) /  &
                                         (omega_geo(igeo2)-omega_geo(igeo1))
 ENDDO
+!
+CALL isobaric_heat_capacity(vmin_t,b0_t,beta_t,temp,cp_t,ntemp)
+!
+!  The routine gives only the difference
+!
+cp_t(1:ntemp) = cp_t(1:ntemp)+cv_t(1:ntemp)
 
-DO itemp=2,ntemp-1
+CALL isoentropic_bulk_modulus(vmin_t,b0_t,beta_t,cp_t,temp,b0_s,ntemp)
+!
+!  The routine gives only the difference
+!
+b0_s(1:ntemp) = b0_s(1:ntemp)+b0_t(1:ntemp)
 
-   cp_t(itemp) = cv_t(itemp) + temp(itemp) * vmin_t(itemp) * &
-                           beta_t(itemp)**2 * b0_t(itemp) / ry_kbar
-
-   b0_s(itemp) = b0_t(itemp) + temp(itemp) * vmin_t(itemp) * &
-                           beta_t(itemp)**2 * b0_t(itemp)**2 / cp_t(itemp) &
-                           / ry_kbar
-
-   gamma_t(itemp) =  beta_t(itemp) * b0_t(itemp) * vmin_t(itemp) / ry_kbar / &
-                     cv_t(itemp) 
-
-END DO
+CALL average_gruneisen(vmin_t,b0_t,beta_t,cv_t,temp,gamma_t,ntemp)
 
 RETURN
 END SUBROUTINE compute_cp
