e62af46bd56bd3c2bab127b3a4ee7957da5bbb68
dalcorso <dalcorso>
2015-05-12 09:22:17 +0000

    Removed duplicated routine.

--- src/polifit.f90
+++ src/polifit.f90
@@ -8,6 +8,7 @@
 
 SUBROUTINE polifit(x,y,ndati,a,m1)
 USE kinds, ONLY : DP
+USE quadratic_surfaces, ONLY : linsolvx
 IMPLICIT NONE
 INTEGER, INTENT(IN)   ::   ndati, &  ! number of data points
                            m1        ! polynomial coefficients 
@@ -36,38 +37,3 @@ CALL linsolvx(amat,m1,bvec,a)
 RETURN
 END SUBROUTINE polifit
 !
-!-------------------------------------------------------------------
-SUBROUTINE linsolvx(hc,n,vc,alpha)
-!-------------------------------------------------------------------
-!
-!    This routine is a driver for the correspondent lapack routines
-!    which solve a linear system of equations with real coefficients. On 
-!    input the matrix is contained in hc, and the known part in vc, on 
-!    output the solution is on alpha.
-!
-!
-USE kinds, ONLY : DP
-IMPLICIT NONE
-
-INTEGER, INTENT(IN)  :: n        ! input: logical dimension of hc
-
-REAL(DP), INTENT(IN)  ::  hc(n,n),  &  ! input: the matrix to solve
-                          vc(n)        ! input: the known part of the system
-
-REAL(DP), INTENT(OUT) ::  alpha(n)     ! output: the solution
-
-INTEGER, ALLOCATABLE    :: iwork(:)
-INTEGER :: info
-
-ALLOCATE(iwork(n))
-
-CALL dgetrf(n,n,hc,n,iwork,info)
-CALL errore('linsolvx','error in factorization',abs(info))
-alpha=vc
-CALL dgetrs('N',n,1,hc,n,iwork,alpha,n,info)
-CALL errore('linsolvx','error in solving',abs(info))
- 
-DEALLOCATE( iwork )
-
-RETURN
-END SUBROUTINE linsolvx
