4f481fc849fa3ad288fc7a7204faf668f63f8f3f
dalcorso <dalcorso>
2015-05-16 10:47:20 +0000

    Cleanup.

--- src/write_gnuplot_energy.f90
+++ src/write_gnuplot_energy.f90
@@ -19,7 +19,6 @@ SUBROUTINE write_gnuplot_energy(nwork)
   USE kinds,      ONLY : DP
   USE thermo_mod, ONLY : energy_geo, ngeo, celldm_geo, omega_geo
   USE cell_base,  ONLY : ibrav
-  USE control_mur, ONLY : lmurn
   USE data_files, ONLY : flenergy
   USE io_global,  ONLY : stdout, meta_ionode, meta_ionode_id
   USE mp_world,   ONLY : world_comm
@@ -50,41 +49,34 @@ SUBROUTINE write_gnuplot_energy(nwork)
         OPEN(UNIT=iu_ev, FILE=TRIM(fileout), STATUS='UNKNOWN', &
                          FORM='FORMATTED', ERR=20, IOSTAT=ios)
 
-        IF (lmurn) THEN
-           DO iwork=1,ngeo(1)
-              WRITE(iu_ev,'(2e30.15)', ERR=20, IOSTAT=ios) &
-                              celldm_geo(1,iwork), energy_geo(iwork) 
-           ENDDO
-        ELSE
-           SELECT CASE (ibrav)
-              CASE(1,2,3)
-                 DO iwork=1,ngeo(1)
-                    WRITE(iu_ev,'(2e30.15)', ERR=20, IOSTAT=ios) &
-                              celldm_geo(1,iwork), energy_geo(iwork) 
-                 ENDDO
-              CASE(4,6,7)
-                 DO iwork=1,ngeo(1)*ngeo(3)
-                     WRITE (iu_ev,'(3e25.12)', ERR=20, IOSTAT=ios) &
-                           celldm_geo(1,iwork), &
-                           celldm_geo(3,iwork), & 
-                           energy_geo(iwork) 
-                 ENDDO
-              CASE(5)
-                 DO iwork=1,ngeo(1)*ngeo(4)
-                    WRITE(iu_ev,'(3e25.12)', ERR=20, IOSTAT=ios) &
-                         celldm_geo(1,iwork), &
-                         celldm_geo(4,iwork), &
-                         energy_geo(iwork) 
-                 ENDDO
-              CASE(8,9,91,10,11)
-                 DO iwork=1+(ifiles-1)*ngeo(1)*ngeo(2), ifiles*ngeo(1)*ngeo(2)
-                    WRITE(iu_ev,'(3e25.12)', ERR=20, IOSTAT=ios) &
+        SELECT CASE (ibrav)
+           CASE(1,2,3)
+              DO iwork=1,ngeo(1)
+                 WRITE(iu_ev,'(2e30.15)', ERR=20, IOSTAT=ios) &
+                           celldm_geo(1,iwork), energy_geo(iwork) 
+              ENDDO
+           CASE(4,6,7)
+              DO iwork=1,ngeo(1)*ngeo(3)
+                  WRITE (iu_ev,'(3e25.12)', ERR=20, IOSTAT=ios) &
                         celldm_geo(1,iwork), &
-                        celldm_geo(2,iwork), energy_geo(iwork)  
-                                          
-                 ENDDO
-           END SELECT
-        ENDIF
+                        celldm_geo(3,iwork), & 
+                        energy_geo(iwork) 
+              ENDDO
+           CASE(5)
+              DO iwork=1,ngeo(1)*ngeo(4)
+                 WRITE(iu_ev,'(3e25.12)', ERR=20, IOSTAT=ios) &
+                      celldm_geo(1,iwork), &
+                      celldm_geo(4,iwork), &
+                      energy_geo(iwork) 
+              ENDDO
+           CASE(8,9,91,10,11)
+              DO iwork=1+(ifiles-1)*ngeo(1)*ngeo(2), ifiles*ngeo(1)*ngeo(2)
+                 WRITE(iu_ev,'(3e25.12)', ERR=20, IOSTAT=ios) &
+                     celldm_geo(1,iwork), &
+                     celldm_geo(2,iwork), energy_geo(iwork)  
+                                       
+              ENDDO
+        END SELECT
         CLOSE(iu_ev)
      ENDDO
      !
