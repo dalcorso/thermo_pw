d4431c467e5183937f1864e0fd7b30db7b7915ed
dalcorso <dalcorso@sissa.it>
2017-05-17 12:05:16 +0200

    Small change of the algorithm that removes from the path the short lines that cannot be plotted.

--- src/clean_path.f90
+++ src/clean_path.f90
@@ -22,23 +22,25 @@ INTEGER, INTENT(IN) :: nqaux
 INTEGER, INTENT(INOUT) :: wqaux(nqaux)
 REAL(DP), INTENT(IN) :: xqaux(3, nqaux)
 
-REAL(DP) :: total_lenght, fact, leng(nqaux)
+REAL(DP) :: total_length, average_length, fact, leng(nqaux)
 INTEGER :: iq
 
-fact=20.0_DP
+IF (nqaux<2) RETURN
+fact=5.0_DP
 
-total_lenght=0.0_DP
+total_length=0.0_DP
 DO iq=1, nqaux-1
    IF (wqaux(iq) > 0) THEN
       leng(iq)= SQRT( (xqaux(1,iq+1) - xqaux(1,iq))**2 + &
                       (xqaux(2,iq+1) - xqaux(2,iq))**2 + &
                       (xqaux(3,iq+1) - xqaux(3,iq))**2 )
-      total_lenght=total_lenght + leng(iq)
+      total_length=total_length + leng(iq)
    ENDIF
 ENDDO
+average_length = total_length / (nqaux-1)
 
 DO iq=1, nqaux-1
-   IF ( leng(iq) * fact < total_lenght ) wqaux(iq)=0
+   IF ( leng(iq) * fact < average_length ) wqaux(iq)=0
 ENDDO
 
 RETURN
