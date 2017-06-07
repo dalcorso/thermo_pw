9fdd030650cc3c0e0eee913db6757a3c4cc5c53c
dalcorso <dalcorso@sissa.it>
2017-06-07 10:37:08 +0200

    Added a few routines to rotate tensors with three or four components.

--- lib/rotate.f90
+++ lib/rotate.f90
@@ -15,6 +15,10 @@ MODULE rotate
 !               rotated vectors.  
 !  rotate_tensors2: takes a set of rank two tensors, a rotation matrix
 !               and gives the rotated rank two tensors.
+!  rotate_tensors3: takes a set of rank three tensors, a rotation matrix
+!               and gives the rotated rank three tensor.
+!  rotate_tensors4: takes a set of rank four tensors, a rotation matrix
+!               and gives the rotated rank four tensors.
 !
 !  rotate_rot : receive a rotation matrix that describe a rotation of
 !               the reference system and a rotation matrix in the old
@@ -29,6 +33,10 @@ MODULE rotate
 !  is_rotation: a function that receives a 3x3 matrix and returns .true.
 !               if it is a rotation matrix
 !
+!  set_rot_xyz: set a rotation matrix of an angle phi about x, y, or z
+!               It sets cos(phi), -sin(phi)
+!                       sin(phi),  cos(phi)
+!               on the axis that are not fix.
 !
   USE kinds,      ONLY : DP
   !
@@ -36,9 +44,8 @@ MODULE rotate
   PRIVATE
   SAVE
 
-
-  PUBLIC rotate_vect, rotate_tensors2, euler_to_su2, find_rotation, &
-         is_rotation, rotate_rot
+  PUBLIC rotate_vect, rotate_tensors2, rotate_tensors3, rotate_tensors4,  &
+         euler_to_su2, find_rotation, is_rotation, rotate_rot, set_rot_xyz
 
 CONTAINS
    SUBROUTINE rotate_vect(rot, n, a, ra, flag)
@@ -120,6 +127,132 @@ CONTAINS
 
    RETURN
    END SUBROUTINE rotate_tensors2
+
+   SUBROUTINE rotate_tensors3(rot, n, a, ra, flag)
+!
+!  this routine applies a rotation to n tensors of rank 4
+!  if flag is 1 rotate the n tensors according to rot
+!  if flag is -1 rotate the n tensors according to rot^-1
+!
+
+   IMPLICIT NONE
+   INTEGER, INTENT(IN) :: n
+   REAL(DP), INTENT(IN) :: rot(3,3), a(3,3,3,n)
+   REAL(DP), INTENT(OUT) :: ra(3,3,3,n)
+   INTEGER, INTENT(IN) :: flag
+
+   INTEGER :: ivect, irot1, irot2, irot3, jrot1, jrot2, jrot3
+  
+   ra=0.0_DP
+   DO ivect=1,n
+      IF (flag==1) THEN
+         DO irot1=1,3
+          DO irot2=1,3
+           DO irot3=1,3
+            DO jrot1=1,3
+             DO jrot2=1,3
+              DO jrot3=1,3
+                ra(irot1,irot2,irot3,ivect)=            &
+                   ra(irot1,irot2,irot3,ivect)+         &
+                       rot(irot1,jrot1)*rot(irot2,jrot2)*     &
+                       rot(irot3,jrot3)*a(jrot1,jrot2,jrot3,ivect)
+              ENDDO
+             ENDDO
+            ENDDO
+           ENDDO
+          ENDDO
+         ENDDO
+      ELSE
+         DO irot1=1,3
+          DO irot2=1,3
+           DO irot3=1,3
+            DO jrot1=1,3
+             DO jrot2=1,3
+              DO jrot3=1,3
+                 ra(irot1,irot2,irot3,ivect)=            &
+                    ra(irot1,irot2,irot3,ivect)+         &
+                        rot(jrot1,irot1)*rot(jrot2,irot2)*     &
+                        rot(jrot3,irot3)*    &
+                                a(jrot1,jrot2,jrot3,ivect)
+              ENDDO
+             ENDDO
+            ENDDO
+           ENDDO
+          ENDDO
+         ENDDO
+      ENDIF
+   END DO
+
+   RETURN
+   END SUBROUTINE rotate_tensors3
+
+   SUBROUTINE rotate_tensors4(rot, n, a, ra, flag)
+!
+!  this routine applies a rotation to n tensors of rank 4
+!  if flag is 1 rotate the n tensors according to rot
+!  if flag is -1 rotate the n tensors according to rot^-1
+!
+
+   IMPLICIT NONE
+   INTEGER, INTENT(IN) :: n
+   REAL(DP), INTENT(IN) :: rot(3,3), a(3,3,3,3,n)
+   REAL(DP), INTENT(OUT) :: ra(3,3,3,3,n)
+   INTEGER, INTENT(IN) :: flag
+
+   INTEGER :: ivect, irot1, irot2, irot3, irot4,   & 
+                     jrot1, jrot2, jrot3, jrot4
+  
+   ra=0.0_DP
+   DO ivect=1,n
+      IF (flag==1) THEN
+         DO irot1=1,3
+          DO irot2=1,3
+           DO irot3=1,3
+            DO irot4=1,3
+             DO jrot1=1,3
+              DO jrot2=1,3
+               DO jrot3=1,3
+                DO jrot4=1,3
+                  ra(irot1,irot2,irot3,irot4,ivect)=            &
+                     ra(irot1,irot2,irot3,irot4,ivect)+         &
+                         rot(irot1,jrot1)*rot(irot2,jrot2)*     &
+                         rot(irot3,jrot3)*rot(irot4,jrot4)*     &
+                                 a(jrot1,jrot2,jrot3,jrot4,ivect)
+                ENDDO
+               ENDDO
+              ENDDO
+             ENDDO
+            ENDDO
+           ENDDO
+          ENDDO
+         ENDDO
+      ELSE
+         DO irot1=1,3
+          DO irot2=1,3
+           DO irot3=1,3
+            DO irot4=1,3
+             DO jrot1=1,3
+              DO jrot2=1,3
+               DO jrot3=1,3
+                DO jrot4=1,3
+                  ra(irot1,irot2,irot3,irot4,ivect)=            &
+                     ra(irot1,irot2,irot3,irot4,ivect)+         &
+                         rot(jrot1,irot1)*rot(jrot2,irot2)*     &
+                         rot(jrot3,irot3)*rot(jrot4,irot4)*     &
+                                 a(jrot1,jrot2,jrot3,jrot4,ivect)
+                ENDDO
+               ENDDO
+              ENDDO
+             ENDDO
+            ENDDO
+           ENDDO
+          ENDDO
+         ENDDO
+      ENDIF
+   END DO
+
+   RETURN
+   END SUBROUTINE rotate_tensors4
 
    SUBROUTINE rotate_rot(rot, rot_in, rot_out)
 
@@ -208,5 +341,42 @@ END DO
 
 RETURN
 END FUNCTION is_rotation
+
+SUBROUTINE set_rot_xyz(rot, axis, angle)
+!
+!  Set a rotation matrix about the axis x, y, or z according to axis (1,2,3).
+!  Angle is the rotation angle and it is supposed to be in radiants
+!
+IMPLICIT NONE
+REAL(DP), INTENT(INOUT) :: rot(3,3)
+REAL(DP), INTENT(IN) :: angle
+INTEGER, INTENT(IN) :: axis
+
+rot=0.0_DP
+
+IF (axis==1) THEN
+   rot(1,1)=1.0_DP
+   rot(2,2)=COS(angle)
+   rot(2,3)=-SIN(angle)
+   rot(3,2)=SIN(angle)
+   rot(3,3)=COS(angle)
+ELSEIF (axis==2) THEN
+   rot(2,2)=1.0_DP
+   rot(1,1)=COS(angle)
+   rot(1,3)=-SIN(angle)
+   rot(3,1)=SIN(angle)
+   rot(3,3)=COS(angle)
+ELSEIF (axis==3) THEN
+   rot(3,3)=1.0_DP
+   rot(1,1)=COS(angle)
+   rot(1,2)=-SIN(angle)
+   rot(2,1)=SIN(angle)
+   rot(2,2)=COS(angle)
+ELSE
+   CALL errore('set_rot_xyz','axis not programmed',1)
+ENDIF
+
+RETURN
+END SUBROUTINE set_rot_xyz
 
 END MODULE rotate
