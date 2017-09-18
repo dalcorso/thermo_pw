62f9f4397c95f788cd5037a751715677de6ced3a
dalcorso <dalcorso@sissa.it>
2017-09-18 16:51:50 +0200

    Added a routine that allocates the same variables as phq_readin when frequency is not used.

--- qe/deallocate_ph_tpw.f90
+++ qe/deallocate_ph_tpw.f90
@@ -5,6 +5,25 @@
 ! in the root directory of the present distribution,
 ! or http://www.gnu.org/copyleft/gpl.txt .
 !
+SUBROUTINE allocate_ph_tpw
+!
+!  This allocation is done by phq_readin_tpw. To avoid crash when only
+!  the xml file is read we allocate these variables here
+!
+USE kinds, ONLY : DP
+USE optical, ONLY : fru
+USE images_omega, ONLY : comp_f
+USE freq_ph, ONLY : fiu
+IMPLICIT NONE
+    IF (.NOT. ALLOCATED(fru)) ALLOCATE(fru(1))
+    IF (.NOT. ALLOCATED(fiu)) ALLOCATE(fiu(1))
+    IF (.NOT. ALLOCATED(comp_f)) ALLOCATE(comp_f(1))
+    fiu=0.0_DP
+    fru=0.0_DP
+    comp_f=.TRUE.
+RETURN
+END SUBROUTINE allocate_ph_tpw
+
 SUBROUTINE deallocate_ph_tpw
 
 USE optical, ONLY : fru
