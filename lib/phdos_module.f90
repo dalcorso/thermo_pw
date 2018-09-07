0d36912d010b6a98ba4647961d370c815b7267ef
dalcorso <dalcorso@sissa.it>
2018-09-07 19:45:47 +0200

    Added a routine to read the generalized phdos in the phdos_module.

--- lib/phdos_module.f90
+++ lib/phdos_module.f90
@@ -62,7 +62,7 @@ PUBLIC :: phdos_type, read_phdos_data, zero_point_energy, free_energy, &
           vib_energy, vib_entropy, specific_heat_cv, fecv, &
           phdos_debye_factor, integrated_dos, set_phdos, destroy_phdos, &
           find_minimum_maximum, gen_phdos_type, set_gen_phdos, &
-          destroy_gen_phdos
+          read_genphdos_data, destroy_gen_phdos
           
 CONTAINS
 
@@ -184,6 +184,86 @@ IF (ionode) CLOSE(iunit)
 
 RETURN
 END SUBROUTINE read_phdos_data
+
+SUBROUTINE read_genphdos_data(phdos, nat, filename)
+!
+!  This subroutine reads the phdos from a file. It allocates space,
+!  opens and closes the phdos file.
+!
+USE mp_images, ONLY : intra_image_comm
+USE io_global, ONLY : ionode_id, ionode, stdout
+USE mp,        ONLY : mp_bcast
+
+IMPLICIT NONE
+INTEGER, INTENT(IN) :: nat
+TYPE(gen_phdos_type), INTENT(INOUT) :: phdos
+CHARACTER(LEN=256), INTENT(IN) :: filename
+INTEGER :: iunit, ios
+INTEGER, PARAMETER :: ndivx=100000
+REAL(DP), ALLOCATABLE :: nu(:), dos(:,:,:)
+REAL(DP) :: de, de_
+INTEGER :: i, ijpol, na, ndiv
+INTEGER :: find_free_unit
+CHARACTER(LEN=256) :: filename_loc
+CHARACTER(LEN=6) :: int_to_char
+
+IF (ionode) iunit=find_free_unit()
+
+ALLOCATE(nu(ndivx))
+ALLOCATE(dos(6,nat,ndivx))
+de = 0d0
+DO na=1,nat
+   filename_loc=TRIM(filename)//'.'//TRIM(int_to_char(na))
+   IF (ionode) OPEN(FILE=TRIM(filename_loc), UNIT=iunit, STATUS='old', &
+                       FORM='formatted', ERR=100, IOSTAT=ios)
+100 CALL mp_bcast(ios, ionode_id, intra_image_comm)
+   IF (ios /= 0) CALL errore('read_phdos_data', &
+                          'opening file'//TRIM(filename_loc), ABS(ios))
+   IF (ionode) THEN
+      DO i=1,ndivx
+    ! nu(i) = frequencies (cm^{-1}), dos(i) in states/cm^{-1} 
+         READ(iunit, *, END=20, ERR=10, IOSTAT=ios) nu(i), &
+                                                (dos(ijpol,na,i),ijpol=1,6)
+         ndiv=i
+      ENDDO
+20    ios=0
+   ENDIF
+10 CALL mp_bcast(ios, ionode_id, intra_image_comm)
+   IF (ios /= 0 ) CALL errore('read_genphdos_data', 'problem reading phdos', 1)
+   IF (ionode) CLOSE(iunit)
+ENDDO
+CALL mp_bcast(ndiv,ionode_id,intra_image_comm)
+CALL mp_bcast(nu,ionode_id,intra_image_comm)
+CALL mp_bcast(dos,ionode_id,intra_image_comm)
+
+DO i=1,ndiv
+   IF ( nu(i) < -1.d0 ) THEN
+      WRITE(stdout,*) i, nu(i), dos(ijpol,1,i)
+      CALL errore('read_genphdos_data','negative frequencies',1)
+   ELSE IF ( nu(i) < 0.d0 ) THEN
+      nu(i) = 0.d0
+   END IF
+   IF ( i ==2 ) THEN
+      de_ = nu(2) - nu(1)
+   ELSEIF (i > 2) THEN
+      de = nu(i) - nu(i-1)
+      IF ( ABS(de - de_) > 1.0d-4 ) &
+               CALL errore('read_genphdos_data','nonuniform grid',1)
+   END IF
+ENDDO
+
+phdos%number_of_points=ndiv
+phdos%de=de
+ALLOCATE(phdos%nu(ndiv))
+ALLOCATE(phdos%phdos(6,nat,ndiv))
+phdos%nu(:) = nu(1:ndiv)
+phdos%phdos(1:6,1:nat,1:ndiv) = dos(1:6,1:nat,1:ndiv)
+
+DEALLOCATE(nu)
+DEALLOCATE(dos)
+
+RETURN
+END SUBROUTINE read_genphdos_data
 
 SUBROUTINE zero_point_energy(phdos, ener)
 !
