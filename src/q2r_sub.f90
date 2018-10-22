21a15ada03594e3f740bbd015705f2380d4bdeaf
dalcorso <dalcorso@sissa.it>
2018-10-22 17:22:17 +0200

    nspin_mag was not read correctly when after_disp=.TRUE. (Bug signaled by A. Urru).

--- src/q2r_sub.f90
+++ src/q2r_sub.f90
@@ -358,7 +358,7 @@ SUBROUTINE q2r_sub(fildyn)
   !
 
   CALL interface_with_tpw(phid, nr1, nr2, nr3, nat, ntyp, lrigid, zeu, &
-                            epsil, atm, m_loc, tau, ityp, at, bg, omega)
+                     epsil, atm, nspin_mag, m_loc, tau, ityp, at, bg, omega)
   !
   DEALLOCATE(nc)
   DEALLOCATE(phid) 
@@ -597,7 +597,8 @@ subroutine set_zasr ( zasr, nr1,nr2,nr3, nat, ibrav, tau, zeu)
  end subroutine set_zasr
 
 SUBROUTINE interface_with_tpw(frc_, nr1, nr2, nr3, nat_, ntyp_, has_zstar_, &
-                zeu_, epsil_, atm_, m_loc_, tau_, ityp_, at_, bg_, omega_ )
+                zeu_, epsil_, atm_, nspin_mag_, m_loc_, tau_, ityp_, at_, &
+                bg_, omega_ )
 !
 !  This routine is used to copy the variables produced by q2r in the variables
 !  of the thermo_pw code, avoiding to read the file on disk.
@@ -607,9 +608,10 @@ USE ifc,    ONLY : frc, atm, zeu, m_loc, epsil_ifc, has_zstar
 USE ions_base, ONLY : nat, ntyp=>nsp, tau, ityp
 USE cell_base, ONLY : at, bg, omega
 USE disp,   ONLY : nq1, nq2, nq3
+USE noncollin_module, ONLY : nspin_mag
 
 IMPLICIT NONE
-INTEGER,     INTENT(IN) :: nr1, nr2, nr3, nat_, ntyp_, ityp_(nat_)
+INTEGER,     INTENT(IN) :: nr1, nr2, nr3, nat_, ntyp_, ityp_(nat_), nspin_mag_
 COMPLEX(DP),    INTENT(IN) :: frc_(nr1*nr2*nr3,3,3,nat_,nat_)
 REAL(DP),    INTENT(IN) :: zeu_(3,3,nat_), m_loc_(3,nat_), epsil_(3,3), &
                            at_(3,3), bg_(3,3), omega_, tau_(3,nat_) 
@@ -641,6 +643,7 @@ DO k=1,nr3
 ENDDO
 zeu=zeu_
 atm=atm_
+nspin_mag=nspin_mag_
 m_loc=m_loc_
 epsil_ifc=epsil_
 has_zstar=has_zstar_
