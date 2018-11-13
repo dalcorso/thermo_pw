bad27160197d8c3de3584943597855412f1c8258
dalcorso <dalcorso@sissa.it>
2018-11-13 19:48:55 +0100

    Small cleanup.

--- src/write_minimum_energy_data.f90
+++ src/write_minimum_energy_data.f90
@@ -16,41 +16,31 @@ SUBROUTINE write_minimum_energy_data()
   USE control_mur,      ONLY : b0, b01, emin, lmurn
   USE equilibrium_conf, ONLY : celldm0
   USE control_pressure, ONLY : pressure_kb
-  USE cell_base,        ONLY : ibrav
   USE io_global,        ONLY : stdout
   IMPLICIT NONE
+  CHARACTER(LEN=20) :: quantity
   !
   WRITE(stdout,'(/,2x,76("-"))')
   IF (pressure_kb /= 0.0_DP) THEN
      WRITE(stdout,'(5x,"At pressure ",f15.6," kbar")') pressure_kb
+     quantity='enthalpy'
   ELSE
      WRITE(stdout,*) 
+     quantity='total energy'
   ENDIF
-  IF (ibrav==1.OR.ibrav==2.OR.ibrav==3) THEN
+  IF (lmurn) THEN
      WRITE(stdout,'(5x, "The equilibrium lattice constant is ",9x,f12.4,&
                                  &" a.u.")') celldm0(1)
-  ELSE
-     IF (pressure_kb /= 0.0_DP) THEN
-        WRITE(stdout,'(5x,"The minimum enthalpy is obtained for celldm")')
-     ELSE
-        WRITE(stdout,'(5x,"The minimum energy is obtained for celldm")')
-     ENDIF
-     WRITE(stdout,'(5x,6f12.5)') celldm0(:)
-  ENDIF
- 
-  IF (lmurn) THEN
      WRITE(stdout,'(5x, "The bulk modulus is ",24x,f12.3,"  kbar")') b0
      WRITE(stdout,'(5x, "The pressure derivative of the bulk modulus is ",&
                                   &f9.3)')  b01
-  END IF
-  IF (pressure_kb /= 0.0_DP) THEN
-     WRITE(stdout,'(5x,"The enthalpy at the minimum is    ",6x,f20.9," Ry")') &
-                                                               emin
   ELSE
-     WRITE(stdout,'(5x,"The total energy at the minimum is",6x,f20.9," Ry")') &
-                                                 emin
-  END IF
-
+     WRITE(stdout,'(5x,"The equilibrium celldm is:")')
+     WRITE(stdout,'(5x,6f12.5)') celldm0(:)
+  ENDIF
+ 
+  WRITE(stdout,'(5x,"The ",a," at the minimum is    ",6x,f20.9," Ry")') &
+                                                   TRIM(quantity), emin
   WRITE(stdout,'(2x,76("-"),/)')
   RETURN
 END SUBROUTINE write_minimum_energy_data
