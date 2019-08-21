46efe0750f735dd4b0aa04ff2a22449def460fe7
dalcorso <dalcorso@sissa.it>
2019-08-21 18:45:54 +0200

    Added a module to deal with multivariate polynomials and a module to interpolate with linear polynomials.

---
+++ lib/linear_surfaces.f90
@@ -0,0 +1,240 @@
+!
+! Copyright (C) 2019 Andrea Dal Corso 
+! This file is distributed under the terms of the
+! GNU General Public License. See the file `License'
+! in the root directory of the present distribution,
+! or http://www.gnu.org/copyleft/gpl.txt .
+!
+MODULE linear_surfaces
+!
+!   This module contains the support routines for dealing with linear
+!   surfaces interpolation. 
+!
+!   It provides the following routines:
+!
+!   fit_multi_linear receives as input the coordinates of some
+!   points and the value of the function in these points and
+!   finds the coefficients of the linear polynomial that better
+!   interpolates all the points.
+!
+!   evaluate_fit_linear evaluates the linear polynomial at a given 
+!   input point.
+!
+!   set_linear_gradient gives the gradient of the linear polynomial.
+!   (which is constant independent on the input point).
+!
+!   print_linear_polynomial writes on output the coefficients of the linear
+!   polynomial.
+!
+!   introduce_linear_fit writes a message with a few information on the
+!   linear polynomial and the number of data used to fit it.
+!
+!   print_chisq_linear writes the sum of the squares of the difference 
+!   between the input data and the interpolating linear polynomial divided
+!   by the number of data points.
+!
+  USE kinds, ONLY : DP
+  USE polynomial, ONLY : poly1
+  USE io_global, ONLY : stdout
+  IMPLICIT NONE
+  PRIVATE
+  SAVE
+
+  PUBLIC :: fit_multi_linear, evaluate_fit_linear, set_linear_grad,  &
+            print_linear_polynomial, introduce_linear_fit, &
+            print_chisq_linear                                        
+
+CONTAINS
+
+SUBROUTINE fit_multi_linear(ndata,nvar,x,f,p_in)
+!
+!  This routine receives as input a set of vectors x(nvar,ndata) and
+!  function values f(ndata) and gives as output the coefficients of
+!  a linear interpolating polynomial a0 and v1(nvar). In input
+!  ndata is the number of data points, nvar is the number of variables.
+!
+!  The coefficients are organized as follows:
+!  p_in%a0 + \sum_j p_in%phi1(j)  x(j,i) 
+!
+!  The number of coefficients necessary for the fit is nvar+1, where
+!  nvar is the number of variables of the polynomial.
+!  To interpolate the function it is better to give to fit_multi_linear
+!  at least as many data points as the number of coefficients or more. The 
+!  routines makes a least square fit of the data.
+!   
+!
+USE linear_solvers, ONLY : linsolvx
+IMPLICIT NONE
+INTEGER, INTENT(IN) :: nvar, ndata
+REAL(DP), INTENT(IN) :: x(nvar,ndata), f(ndata)
+TYPE(poly1), INTENT(INOUT) :: p_in
+
+REAL(DP), ALLOCATABLE :: amat(:,:), aa(:,:), b(:), coeff(:) 
+
+INTEGER :: ivar, jvar, idata, i, ncoeff
+
+IF (nvar<1) CALL errore('fit_multi_linear','nvar must be larger than 1',1)
+IF (ndata < nvar+1) &
+   CALL errore('fit_multi_linear','Be careful: there are too few &
+                                                        &sampling data',1)
+ncoeff=nvar+1
+ALLOCATE(amat(ndata,ncoeff))
+ALLOCATE(coeff(ncoeff))
+ALLOCATE(aa(ncoeff,ncoeff))
+ALLOCATE(b(ncoeff))
+!
+!  prepare the auxiliary matrix
+!
+amat=0.0_DP
+
+DO idata=1,ndata
+   amat(idata,1) = 1.0_DP
+   DO i=1,nvar
+      amat(idata,1+i) = x(i,idata)
+   ENDDO
+ENDDO
+!
+!   Now set the linear system
+!
+aa=0.0_DP
+b =0.0_DP
+DO ivar=1,ncoeff
+   DO jvar=1,ncoeff
+      DO idata=1,ndata
+         aa(ivar,jvar)= aa(ivar,jvar) + amat(idata,ivar) * amat(idata,jvar)
+      END DO
+   END DO
+   DO idata=1,ndata
+      b(ivar) = b(ivar) + amat(idata,ivar) * f(idata)
+   END DO
+END DO
+!
+!   solve the linear system and find the coefficients
+!
+CALL linsolvx(aa,ncoeff,b,coeff)
+!
+!   assign the coefficients to the polynomial variables
+!
+p_in%a0=coeff(1)
+DO i=1,nvar
+   p_in%phi1(i)=coeff(1+i)
+ENDDO
+
+DEALLOCATE(amat)
+DEALLOCATE(coeff)
+DEALLOCATE(aa)
+DEALLOCATE(b)
+
+RETURN
+END SUBROUTINE fit_multi_linear
+!
+SUBROUTINE evaluate_fit_linear(nvar,x,f,p_in)
+!
+!  This routine evaluates the linear polynomial at the point x
+!
+IMPLICIT NONE
+INTEGER, INTENT(IN) :: nvar
+REAL(DP), INTENT(IN) :: x(nvar)
+TYPE(poly1), INTENT(IN) :: p_in
+REAL(DP), INTENT(OUT) :: f
+
+INTEGER  :: i
+
+f=p_in%a0
+DO i=1, nvar
+   f=f+p_in%phi1(i)*x(i)
+ENDDO
+
+RETURN
+END SUBROUTINE evaluate_fit_linear
+
+SUBROUTINE set_linear_grad(nvar,f,p_in)
+!
+!   This routine gives the gradient of the linear polynomial.
+!
+IMPLICIT NONE
+INTEGER, INTENT(IN) :: nvar
+TYPE(poly1), INTENT(IN) :: p_in
+REAL(DP), INTENT(INOUT) :: f(nvar)
+
+f=p_in%phi1
+
+RETURN
+END SUBROUTINE set_linear_grad
+!
+SUBROUTINE print_linear_polynomial(nvar, p_in)
+!
+!  This routine writes on output the coefficients of the linear polynomial. 
+!
+IMPLICIT NONE
+
+INTEGER, INTENT(IN)     :: nvar
+TYPE(poly1), INTENT(IN) :: p_in
+
+CHARACTER(LEN=6) :: int_to_char
+INTEGER :: i
+
+WRITE(stdout,'(/,5x,"Linear polynomial:")') 
+!
+!  term of 0 degree
+!
+WRITE(stdout,'(5x,e15.7)') p_in%a0
+!
+!  terms of 1 degree
+!
+DO i=1,nvar
+   WRITE(stdout,'(5x," +",e15.7,a)') p_in%phi1(i), '  x'//TRIM(int_to_char(i))
+ENDDO
+
+RETURN
+END SUBROUTINE print_linear_polynomial
+
+SUBROUTINE introduce_linear_fit(nvar, ndata)
+!
+!   This routine writes a small message with the information on 
+!   the fit with a linear polynomial.
+!
+IMPLICIT NONE
+INTEGER, INTENT(IN) :: nvar, ndata
+
+  WRITE(stdout,'(/,5x,"Fitting the data with a linear polynomial:")')
+
+  WRITE(stdout,'(/,5x,"Number of variables:",10x,i5)')  nvar
+  WRITE(stdout,'(5x,"Coefficients of the linear polynomial:",i5)')  nvar+1
+  WRITE(stdout,'(5x,"Number of fitting data:",7x,i5,/)')  ndata
+
+RETURN
+END SUBROUTINE introduce_linear_fit
+
+SUBROUTINE print_chisq_linear(ndata, nvar, x, f, p_in)
+!
+!   This routine receives as input the values of a function f for ndata
+!   values of the independent variables x, a set of coefficients
+!   of a linear interpolating polynomial p_in and writes as output
+!   the sum of the squares of the differences between the values of
+!   the function and of the interpolating polynomial divided by the
+!   number of data.
+!
+IMPLICIT NONE
+
+INTEGER     :: ndata, nvar
+REAL(DP)    :: x(nvar, ndata), f(ndata)
+TYPE(poly1) :: p_in
+
+REAL(DP) :: chisq, perc, aux
+INTEGER  :: idata
+
+chisq=0.0_DP
+perc=0.0_DP
+DO idata=1,ndata
+   CALL evaluate_fit_linear(nvar,x(1,idata),aux,p_in)
+!  WRITE(stdout,'(3f19.12)') f(idata), aux, f(idata)-aux
+   chisq = chisq + (aux - f(idata))**2
+   IF (ABS(f(idata))>1.D-12) perc= perc + ABS((f(idata)-aux) / f(idata))
+ENDDO
+WRITE(stdout,'(5x,"chi square linear=",e18.5," relative error",e18.5,&
+                                     &" %",/)') chisq/ndata, perc*100 / ndata
+RETURN
+END SUBROUTINE print_chisq_linear
+
+END MODULE linear_surfaces
---
+++ lib/polynomial.f90
@@ -0,0 +1,279 @@
+!
+! Copyright (C) 2019 A. Dal Corso
+! This file is distributed under the terms of the
+! GNU General Public License. See the file `License'
+! in the root directory of the present distribution,
+! or http://www.gnu.org/copyleft/gpl.txt .
+!
+
+MODULE polynomial
+!
+!  This module contains the low level definitions of polynomials.
+!  It is used by the linear_surfaces, quadratic_surfaces, cubic_surfaces,
+!  quartic_surfaces modules.
+!  It can deal with polynomial of any number of variables but it is
+!  currently limited to polynomial of up to 4 degree.
+!
+!  It defines the following types:
+!  poly1   A polynomial of degree 1
+!  poly2   A polynomial of degree 2
+!  poly3   A polynomial of degree 3
+!  poly4   A polynomial of degree 4
+!
+!  init_poly Is an interface that receives a polynomial of degree 1, 2, 3, or
+!            4 and calls the routine needed to initialize the polynomial.
+!
+!  clean_poly Is an interface that receives a polynomial of degree 1, 2, 3, or
+!            4 and deallocates its variables.
+!  
+!  poly_ncoeff : receives the number of variables of the polynomial and its
+!                degree d and gives the number of coefficients of a polynomial
+!                that has all terms up to those of degree d
+!
+!  poly_ncoeff_sep : receives the number of variables of the polynomial and
+!                 the degree d and gives the number of coeffiecients of a
+!                 polynomial that has only the terms of degree d.
+!
+USE kinds, ONLY : DP
+IMPLICIT NONE
+PRIVATE
+SAVE
+
+TYPE poly1
+   INTEGER  :: nvar                  ! the number of variables
+   REAL(DP) :: a0                    ! the zero degree coefficient
+   REAL(DP), ALLOCATABLE :: phi1(:)  ! coefficients of the linear polynomial
+END TYPE poly1
+
+TYPE poly2
+   INTEGER  :: nvar                  ! number of variables
+   INTEGER  :: ncoeff2               ! number of coefficients of the quadratic 
+                                     ! polynomial
+   REAL(DP) :: a0                    ! the zero degree coefficient
+   REAL(DP), ALLOCATABLE :: phi1(:)  ! coefficients of the linear polynomial
+   REAL(DP), ALLOCATABLE :: phi2(:)  ! coefficients of the quadratic polynomial
+END TYPE poly2
+
+TYPE poly3
+   INTEGER  :: nvar                  ! number of variables
+   INTEGER  :: ncoeff2               ! number of coefficients of the quadratic
+                                     ! polynomial
+   INTEGER  :: ncoeff3               ! number of coefficients of the cubic
+                                     ! polynomial 
+   REAL(DP) :: a0                    ! the zero degree coefficient
+   REAL(DP), ALLOCATABLE :: phi1(:)  ! coefficients of the linear polynomial
+   REAL(DP), ALLOCATABLE :: phi2(:)  ! coefficients of the quadratic polynomial
+   REAL(DP), ALLOCATABLE :: phi3(:)  ! coefficients of the cubic polynomial
+END TYPE poly3
+
+TYPE poly4
+   INTEGER  :: nvar                  ! number of variables
+   INTEGER  :: ncoeff2               ! number of coefficients of the quadratic
+                                     ! polynomial
+   INTEGER  :: ncoeff3               ! number of coefficients of the cubic
+                                     ! polynomial
+   INTEGER  :: ncoeff4               ! number of coefficients of the quartic
+                                     ! polynomial
+   REAL(DP) :: a0                    ! the zero degree coefficient
+   REAL(DP), ALLOCATABLE :: phi1(:)  ! coefficients of the linear polynomial
+   REAL(DP), ALLOCATABLE :: phi2(:)  ! coefficients of the quadratic polynomial
+   REAL(DP), ALLOCATABLE :: phi3(:)  ! coefficients of the cubic polynomial
+   REAL(DP), ALLOCATABLE :: phi4(:)  ! coefficients of the quartic polynomial
+END TYPE poly4
+
+PUBLIC :: poly1, poly2, poly3, poly4, init_poly, clean_poly, poly_ncoeff, &
+          poly_ncoeff_sep
+
+INTERFACE init_poly
+
+   MODULE PROCEDURE init_poly1, &
+                    init_poly2, &
+                    init_poly3, &
+                    init_poly4  
+END INTERFACE init_poly
+
+INTERFACE clean_poly
+
+   MODULE PROCEDURE clean_poly1, &
+                    clean_poly2, &
+                    clean_poly3, &
+                    clean_poly4
+END INTERFACE clean_poly
+
+CONTAINS
+!
+!   The following four routines allocate the variables of a polynomial of
+!   degree 1, 2, 3, or 4.
+!
+   SUBROUTINE init_poly1(nvar,poly_in)
+   IMPLICIT NONE
+   INTEGER, INTENT(IN) :: nvar
+   TYPE(poly1), INTENT(INOUT) :: poly_in
+
+   poly_in%nvar=nvar
+   ALLOCATE(poly_in%phi1(nvar))
+
+   RETURN
+   END SUBROUTINE init_poly1
+!
+! --------------------------------------------------------------------------
+!
+   SUBROUTINE init_poly2(nvar,poly_in)
+   IMPLICIT NONE
+   INTEGER, INTENT(IN) :: nvar
+   TYPE(poly2), INTENT(INOUT) :: poly_in
+
+   poly_in%nvar=nvar
+   CALL poly_ncoeff_sep(nvar,2,poly_in%ncoeff2)
+   ALLOCATE(poly_in%phi1(nvar))
+   ALLOCATE(poly_in%phi2(poly_in%ncoeff2))
+
+   RETURN
+   END SUBROUTINE init_poly2
+!
+! --------------------------------------------------------------------------
+!
+   SUBROUTINE init_poly3(nvar,poly_in)
+   IMPLICIT NONE
+   INTEGER, INTENT(IN) :: nvar
+   TYPE(poly3), INTENT(INOUT) :: poly_in
+
+   poly_in%nvar=nvar
+   CALL poly_ncoeff_sep(nvar,2,poly_in%ncoeff2)
+   CALL poly_ncoeff_sep(nvar,3,poly_in%ncoeff3)
+   ALLOCATE(poly_in%phi1(poly_in%nvar))
+   ALLOCATE(poly_in%phi2(poly_in%ncoeff2))
+   ALLOCATE(poly_in%phi3(poly_in%ncoeff3))
+
+   RETURN
+   END SUBROUTINE init_poly3
+!
+! --------------------------------------------------------------------------
+!
+   SUBROUTINE init_poly4(nvar,poly_in)
+   IMPLICIT NONE
+   INTEGER, INTENT(IN) :: nvar
+   TYPE(poly4), INTENT(INOUT) :: poly_in
+
+   poly_in%nvar=nvar
+   CALL poly_ncoeff_sep(nvar,2,poly_in%ncoeff2)
+   CALL poly_ncoeff_sep(nvar,3,poly_in%ncoeff3)
+   CALL poly_ncoeff_sep(nvar,4,poly_in%ncoeff4)
+   ALLOCATE(poly_in%phi1(nvar))
+   ALLOCATE(poly_in%phi2(poly_in%ncoeff2))
+   ALLOCATE(poly_in%phi3(poly_in%ncoeff3))
+   ALLOCATE(poly_in%phi4(poly_in%ncoeff4))
+
+   RETURN
+   END SUBROUTINE init_poly4
+!
+! --------------------------------------------------------------------------
+!
+!   The following four routines deallocate the variables of a polynomial of
+!   degree 1, 2, 3, or 4.
+!
+   SUBROUTINE clean_poly1(poly_in)
+   IMPLICIT NONE
+   TYPE(poly1), INTENT(INOUT) :: poly_in
+
+   IF (ALLOCATED(poly_in%phi1)) DEALLOCATE(poly_in%phi1)
+
+   RETURN
+   END SUBROUTINE clean_poly1
+!
+! --------------------------------------------------------------------------
+!
+   SUBROUTINE clean_poly2(poly_in)
+   IMPLICIT NONE
+   TYPE(poly2), INTENT(INOUT) :: poly_in
+
+   IF (ALLOCATED(poly_in%phi1)) DEALLOCATE(poly_in%phi1)
+   IF (ALLOCATED(poly_in%phi2)) DEALLOCATE(poly_in%phi2)
+
+   RETURN
+   END SUBROUTINE clean_poly2
+!
+! --------------------------------------------------------------------------
+!
+   SUBROUTINE clean_poly3(poly_in)
+   IMPLICIT NONE
+   TYPE(poly3), INTENT(INOUT) :: poly_in
+
+   IF (ALLOCATED(poly_in%phi1)) DEALLOCATE(poly_in%phi1)
+   IF (ALLOCATED(poly_in%phi2)) DEALLOCATE(poly_in%phi2)
+   IF (ALLOCATED(poly_in%phi3)) DEALLOCATE(poly_in%phi3)
+
+   RETURN
+   END SUBROUTINE clean_poly3
+!
+! --------------------------------------------------------------------------
+!
+   SUBROUTINE clean_poly4(poly_in)
+   IMPLICIT NONE
+   TYPE(poly4), INTENT(INOUT) :: poly_in
+
+   IF (ALLOCATED(poly_in%phi1)) DEALLOCATE(poly_in%phi1)
+   IF (ALLOCATED(poly_in%phi2)) DEALLOCATE(poly_in%phi2)
+   IF (ALLOCATED(poly_in%phi3)) DEALLOCATE(poly_in%phi3)
+   IF (ALLOCATED(poly_in%phi4)) DEALLOCATE(poly_in%phi4)
+
+   RETURN
+   END SUBROUTINE clean_poly4
+!
+! --------------------------------------------------------------------------
+!
+   SUBROUTINE poly_ncoeff_sep(n,d,ncoeff)
+!
+!  This routine gives the number of coefficients of a polynomial of n
+!  variables of degree d (only the terms of degree d are accounted for).
+!
+   IMPLICIT NONE
+
+   INTEGER, INTENT(IN) :: n, d
+   INTEGER, INTENT(OUT) :: ncoeff
+   INTEGER, ALLOCATABLE :: nmat(:,:)
+   INTEGER :: ni, di, dj
+
+   ALLOCATE(nmat(n,0:d))
+
+   nmat(:,:)=0
+   nmat(1,:)=1
+   DO ni=2, n
+      DO di=0, d
+         DO dj=0, di
+            nmat(ni,di)=nmat(ni,di) + nmat(ni-1,dj)
+         ENDDO
+      ENDDO
+   ENDDO
+
+   ncoeff=nmat(n,d)
+   DEALLOCATE(nmat)
+
+   RETURN
+   END SUBROUTINE poly_ncoeff_sep
+!
+! --------------------------------------------------------------------------
+!
+   SUBROUTINE poly_ncoeff(n,d,ncoeff)
+!
+!  This routine gives the total number of coefficients of a polynomial of
+!  n variables of degree d (all terms of degree up to d are accounted for).
+!
+   IMPLICIT NONE
+
+   INTEGER, INTENT(IN) :: n, d
+   INTEGER, INTENT(OUT) :: ncoeff
+   INTEGER :: ni, di, nsum
+
+   nsum=0
+   DO di=0,d
+      CALL poly_ncoeff_sep(n,di,ni)
+      nsum=nsum+ni
+   ENDDO
+
+   ncoeff=nsum
+
+   RETURN
+   END SUBROUTINE poly_ncoeff
+
+END MODULE polynomial
