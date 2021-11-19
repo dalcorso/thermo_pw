f6a753b501f63f7f8d667df9905315be1fa36b3a
dalcorso <dalcorso@sissa.it>
2021-11-19 10:51:31 +0100

    Added routines to compute arbitrary derivatives of a polynomial of one variable.

--- lib/polyfit.f90
+++ lib/polyfit.f90
@@ -22,6 +22,18 @@ MODULE polyfit_mod
 !  compute_poly_deriv : computes the value of the first derivative of the
 !                 polynomial in one point
 !
+!  compute_poly_deriv2 : computes the value of the second derivative of the
+!                 polynomial in one point
+!
+!  compute_poly_deriv3 : computes the value of the third derivative of the
+!                 polynomial in one point
+!
+!  compute_poly_deriv4 : computes the value of the fourth derivative of the
+!                 polynomial in one point
+!
+!  compute_poly_derivp : computes the value of the p-th derivative of the
+!                 polynomial in one point
+!
 !  write_poly : writes on output the coefficients of the polynomial.
 !
   USE kinds, ONLY : DP
@@ -30,7 +42,9 @@ MODULE polyfit_mod
   PRIVATE
   SAVE
 
-  PUBLIC :: polyfit, compute_poly, compute_poly_deriv, write_poly
+  PUBLIC :: polyfit, compute_poly, compute_poly_deriv, compute_poly_deriv2, &
+            compute_poly_deriv3, compute_poly_deriv4, compute_poly_derivp,  &
+            write_poly
 
 CONTAINS
 
@@ -117,6 +131,127 @@ ENDDO
 
 RETURN
 END SUBROUTINE compute_poly_deriv
+!
+!--------------------------------------------------------------------------
+SUBROUTINE compute_poly_deriv2(x, degree, poly, g)
+!--------------------------------------------------------------------------
+!
+! gives as output the second derivative of the polynomial with respect to x
+! 
+USE kinds, ONLY : DP
+IMPLICIT NONE
+INTEGER, INTENT(IN) :: degree
+REAL(DP), INTENT(IN) :: poly(degree+1), x
+
+REAL(DP), INTENT(OUT) :: g
+
+INTEGER :: i, ncoeff
+
+g=0.0_DP
+IF (degree<2) RETURN
+
+ncoeff=degree+1
+
+g=poly(ncoeff)*(ncoeff-1.0_DP)*(ncoeff-2.0_DP)
+DO i=ncoeff-1,3,-1
+   g = poly(i)*(i-1.0_DP)*(i-2.0_DP) + g*x
+ENDDO
+
+RETURN
+END SUBROUTINE compute_poly_deriv2
+!
+!--------------------------------------------------------------------------
+SUBROUTINE compute_poly_deriv3(x, degree, poly, g)
+!--------------------------------------------------------------------------
+!
+! gives as output the third derivative of the polynomial with respect to x
+! 
+USE kinds, ONLY : DP
+IMPLICIT NONE
+INTEGER, INTENT(IN) :: degree
+REAL(DP), INTENT(IN) :: poly(degree+1), x
+
+REAL(DP), INTENT(OUT) :: g
+
+INTEGER :: i, ncoeff
+
+g=0.0_DP
+IF (degree<3) RETURN
+
+ncoeff=degree+1
+
+g=poly(ncoeff)*(ncoeff-1.0_DP)*(ncoeff-2.0_DP)*(ncoeff-3.0_DP)
+DO i=ncoeff-1,4,-1
+   g = poly(i)*(i-1.0_DP)*(i-2.0_DP)*(i-3.0_DP) + g*x
+ENDDO
+
+RETURN
+END SUBROUTINE compute_poly_deriv3
+!
+!--------------------------------------------------------------------------
+SUBROUTINE compute_poly_deriv4(x, degree, poly, g)
+!--------------------------------------------------------------------------
+!
+! gives as output the fourth derivative of the polynomial with respect to x
+! 
+USE kinds, ONLY : DP
+IMPLICIT NONE
+INTEGER, INTENT(IN) :: degree
+REAL(DP), INTENT(IN) :: poly(degree+1), x
+
+REAL(DP), INTENT(OUT) :: g
+
+INTEGER :: i, ncoeff
+
+g=0.0_DP
+IF (degree<4) RETURN
+
+ncoeff=degree+1
+
+g=poly(ncoeff)*(ncoeff-1.0_DP)*(ncoeff-2.0_DP)*(ncoeff-3.0_DP)&
+                                              *(ncoeff-4.0_DP)
+DO i=ncoeff-1,5,-1
+   g = poly(i)*(i-1.0_DP)*(i-2.0_DP)*(i-3.0_DP)*(i-4.0_DP) + g*x
+ENDDO
+
+RETURN
+END SUBROUTINE compute_poly_deriv4
+!
+!--------------------------------------------------------------------------
+SUBROUTINE compute_poly_derivp(x, degree, p, poly, g)
+!--------------------------------------------------------------------------
+!
+! gives as output the p-th derivative of the polynomial with respect to x
+! 
+USE kinds, ONLY : DP
+IMPLICIT NONE
+INTEGER, INTENT(IN) :: degree, p
+REAL(DP), INTENT(IN) :: poly(degree+1), x
+
+REAL(DP), INTENT(OUT) :: g
+
+INTEGER :: i, j, ncoeff
+REAL(DP) :: aux(degree+1)
+
+g=0.0_DP
+IF (degree<p) RETURN
+
+ncoeff=degree+1
+
+aux(1:ncoeff)=poly(1:ncoeff)
+DO j=1,p
+   DO i=p+1, ncoeff
+      aux(i)=aux(i) * (i-j)
+   ENDDO
+ENDDO
+
+g=aux(ncoeff)
+DO i=ncoeff-1,p+1,-1
+   g = aux(i) + g*x
+ENDDO
+
+RETURN
+END SUBROUTINE compute_poly_derivp
 
 !--------------------------------------------------------------------------
 SUBROUTINE write_poly(a,degree)
