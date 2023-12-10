0184dd8752ac08c5ada04d8d969802c2ba6da66b
dalcorso <dalcorso@sissa.it>
2023-12-10 11:35:51 +0100

    Added the plot of the sound wave velocity surfaces to elastic.f90 (Contributed by X. Gong).

--- tools/elastic.f90
+++ tools/elastic.f90
@@ -40,13 +40,24 @@ PROGRAM elastic
 !  If you do not know set nat to 0.
 !
 USE kinds, ONLY : DP
+USE constants, ONLY : pi
 USE mp_global,        ONLY : mp_startup, mp_global_end
 USE environment,      ONLY : environment_start, environment_end
 USE elastic_constants, ONLY : print_elastic_constants,     &
                               compute_elastic_compliances, &
                               print_elastic_compliances,   &
                               print_macro_elasticity,      &
-                              print_sound_velocities
+                              print_sound_velocities,      &
+                              compute_sound
+
+USE voigt,             ONLY : to_voigt4 
+USE gnuplot,           ONLY : gnuplot_start, gnuplot_end,  &
+                              gnuplot_write_header,        &
+                              gnuplot_ylabel,              &
+                              gnuplot_xlabel,              &
+                              gnuplot_write_command,       &
+                              gnuplot_write_file_mul_data
+
 USE debye_module, ONLY : compute_debye_temperature, debye_cv, &
                          debye_vib_energy, debye_free_energy, debye_entropy, &
                          compute_debye_temperature_poisson, debye_e0, &
@@ -60,11 +71,23 @@ REAL(DP) :: el_con(6,6)          ! the elastic constants
 REAL(DP) :: el_compliances(6,6)  ! the elastic constants
 REAL(DP) :: density, omega, debye_t, approx_debye_t, poisson, bulkm, deltat
 REAL(DP) :: macro_el(8), vp, vb, vg, deb_e0, debye_macro_el
-INTEGER :: i, ntemp, iundeb, ios
+INTEGER :: i, ntemp, iundeb, ios, iunsound
 INTEGER :: find_free_unit, stdin
 REAL(DP), ALLOCATABLE :: temp(:), deb_cv(:), deb_energy(:), &
                          deb_free_energy(:), deb_entropy(:)
 CHARACTER(LEN=9) :: code='elastic'
+
+LOGICAL :: flag
+INTEGER :: l
+INTEGER :: ichoice
+INTEGER, PARAMETER :: lmax=101
+REAL(DP) :: q1, q2, sound_speeds(3,lmax), sound_disp(3,3), theta, factor, &
+            qvec(3), el_con3(3,3,3,3)
+
+CHARACTER(LEN=256) :: filename, filename1, gnu_filename
+INTEGER :: ierr, system
+CHARACTER(LEN=8) :: flext
+CHARACTER(LEN=80) :: gnuplot_command
 
 CALL mp_startup ( start_images=.true. )
 CALL environment_start ( code )
@@ -389,6 +412,9 @@ IF (density > 0.0_DP) THEN
       WRITE(stdout,'(5x,"Volume per cell (a.u.)^3?")')
       READ(stdin,*) omega
    END IF
+
+   WRITE(stdout,'(5x,"Plane to plot? (1: yz, 2: xz, 3: xy)")')
+   READ(stdin,*) ichoice
 END IF
 
 CALL print_elastic_constants(el_con, .FALSE.)
@@ -472,6 +498,89 @@ IF (density > 0.0_DP) THEN
       DEALLOCATE(deb_entropy)
       DEALLOCATE(deb_free_energy)
    END IF
+!
+!  Contributed by X. Gong
+!
+   flag = .false. ! from 6*6 to 3*3*3*3
+   call to_voigt4(el_con, el_con3, flag)
+
+   DO l=1,lmax,1
+      theta = 2.0_DP*pi*(l-1)/(lmax-1)
+      q1 = cos(theta)
+      q2 = sin(theta)
+      ! z1z2 surface q = (q1,q2,0)
+      IF (ichoice == 3) THEN 
+         qvec=(/q1,q2,0.0_DP/) 
+      ELSEIF (ichoice == 2) THEN 
+      ! z2z3 surface q = (0,q2,q3)
+         qvec=(/q1,0.0_DP,q2/)
+      ELSEIF (ichoice == 1) THEN 
+         qvec=(/0.0_DP,q1,q2/)
+      ELSE 
+         CALL errore("elastic", "ichoice must be 1, 2 or 3", 1)
+      ENDIF
+      CALL compute_sound(el_con3, qvec, density, sound_speeds(1,l), sound_disp)
+   ENDDO
+
+   filename1="velocity_surfaces.dat"
+
+   IF (ionode) THEN
+      iunsound=find_free_unit()
+      OPEN(UNIT=iunsound, FILE=filename1, STATUS='unknown', &
+                          FORM='formatted', IOSTAT=ios)
+      factor = 1.D-3
+      DO l=1,lmax,1
+         theta = 2.0_DP*pi*(l-1)/(lmax-1)
+         q1 = cos(theta)
+         q2 = sin(theta)
+         WRITE(iunsound,*) theta, sound_speeds(1,l)*factor,        &
+         sound_speeds(2,l)*factor, sound_speeds(3,l)*factor,       &
+         sound_speeds(1,l)*factor*q1, sound_speeds(1,l)*factor*q2, &
+         sound_speeds(2,l)*factor*q1, sound_speeds(2,l)*factor*q2, &
+         sound_speeds(3,l)*factor*q1, sound_speeds(3,l)*factor*q2
+      ENDDO
+      CLOSE(UNIT=iunsound, STATUS='KEEP')
+   ENDIF
+
+   gnu_filename="plot_velocity_surfaces.gnu"
+   filename="velocity_surfaces.ps"
+   CALL gnuplot_start(gnu_filename)
+
+   flext = ".ps"
+   CALL gnuplot_write_header(filename, 0.0_DP, 0.0_DP, 0.0_DP, 0.0_DP, &
+                            1.0_DP, flext ) 
+   CALL gnuplot_write_command("set size ratio     -1",.FALSE.)
+   CALL gnuplot_write_command("set noborder",.FALSE.)
+   CALL gnuplot_write_command("set xzeroaxis",.FALSE.)
+   CALL gnuplot_write_command("set yzeroaxis",.FALSE.)
+   CALL gnuplot_write_command("set xtics axis",.FALSE.)     
+   CALL gnuplot_write_command("set ytics axis",.FALSE.)
+   CALL gnuplot_write_command("set xtics add ("""" 0)",.FALSE.)  
+   CALL gnuplot_write_command("set ytics add ("""" 0)",.FALSE.)
+   CALL gnuplot_write_command("set arrow 1 from 0,0 to graph 1, &
+                                             &first 0 filled head",.FALSE.)
+   CALL gnuplot_write_command("set arrow 2 from 0,0 to first 0, &
+                                             &graph 1 filled head",.FALSE.)
+   CALL gnuplot_write_command("set arrow 3 from 0,0 to graph 0, &
+                                             &first 0 filled nohead",.FALSE.)
+   CALL gnuplot_write_command("set arrow 4 from 0,0 to first 0, &
+                                             &graph 0 filled nohead",.FALSE.)
+   CALL gnuplot_write_file_mul_data(filename1,5,6,'color_red', & 
+                                              .TRUE.,.FALSE., .FALSE.)
+   CALL gnuplot_write_file_mul_data(filename1,7,8,'color_green',&
+                                              .FALSE.,.FALSE., .FALSE.)
+   CALL gnuplot_write_file_mul_data(filename1,9,10,'color_blue',&
+                                              .FALSE.,.TRUE., .FALSE.)
+   CALL gnuplot_end()
+
+   gnuplot_command="gnuplot"
+
+   IF (ionode) &
+      ierr=system(TRIM(gnuplot_command)//' '//TRIM(gnu_filename))
+
+   !IF (lgnuplot.AND.ionode) &
+   !   CALL EXECUTE_COMMAND_LINE(TRIM(gnuplot_command)//' '&
+   !                                       //TRIM(gnu_filename), WAIT=.FALSE.)
 END IF
 
 CALL environment_end( code )
